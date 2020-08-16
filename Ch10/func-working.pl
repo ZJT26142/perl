2de1347339a5b080936bd8c49fdad33779d330d8
ZJT26142 <46316256+ZJT26142@users.noreply.github.com>
2020-08-16 17:09:30 -0400

    functions

--- Ch10/func-working.pl
+++ Ch10/func-working.pl
@@ -3,14 +3,23 @@
 
 use 5.18.0;
 use warnings;
+use feature 'state';
 
+use subs qw( func ); #pragma takes a list of the name of the functions ; use subs ('func')
 
 my @array = qw( one two three four five );
-func('Fred');
+func 'foo', 'bar', 'baz';
+func0('Fred');
 func1('Fred');
 func2('Fred', 'Barney', 'Wilma', 'Betty', @array);
+say foreach func3();
+func4();
 
 sub func {
+    say foreach @_;
+}
+
+sub func0 {
     my @name = shift;
     say @name;
     say 'Say hello to '.$name[0];
@@ -23,4 +32,13 @@ sub func1 {
 
 sub func2 {
     say foreach @_;
+}
+
+sub func3 {
+    return ( 1, 2, 3, 4, 5 );
+}
+
+sub func4 {
+    state $n = 10; #static var
+    say ++$n;
 }
--- Ch10/functions.md
+++ Ch10/functions.md
@@ -2,3 +2,8 @@
 ## Defining and Calling Functions
 ## Calling a Function with Arguments
 * perl access function arguments
+  * need more reading
+## Locally Scoped Variables
+## Returning Values
+## Static Variables
+## Predeclared Functions
